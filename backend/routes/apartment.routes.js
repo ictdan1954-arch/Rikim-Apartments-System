2a8bb4c1ba7f12e0149af965d8fee0fe298c8e60
ictdan1954-arch <ictdan1954@gmail.com>
2026-07-17 22:18:43 +0300

    Create apartment.routes.js

---
+++ backend/routes/apartment.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const apartmentController = require('../controllers/apartment.controller');
+const authenticate = require('../middleware/auth.middleware');
+const authorize = require('../middleware/role.middleware');
+
+router.use(authenticate);
+
+// Landlord routes
+router.post('/', authorize('landlord'), apartmentController.create);
+router.put('/:id', authorize('landlord'), apartmentController.update);
+router.delete('/:id', authorize('landlord'), apartmentController.delete);
+router.post('/:apartmentId/caretakers', authorize('landlord'), apartmentController.assignCaretaker);
+router.get('/:apartmentId/caretakers', authorize('landlord', 'caretaker'), apartmentController.getCaretakers);
+router.delete('/caretakers/:assignmentId', authorize('landlord'), apartmentController.removeCaretaker);
+
+// All authenticated users
+router.get('/', apartmentController.getAll);
+router.get('/:id', apartmentController.getById);
+
+module.exports = router;
